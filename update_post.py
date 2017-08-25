d5f25190e4cc5a7c3759c4dc908214bd4e00e1f1
sujaykhandekar <31131069+sujaykhandekar@users.noreply.github.com>
2017-08-25 12:31:57 +0530

    Add files via upload

---
+++ update_post.py
@@ -0,0 +1,20 @@
+import xml.etree.cElementTree as ET
+from collections import defaultdict
+import re
+import pprint
+
+def update_postcode(postcode):
+    search = re.search(r'^\D*(\d{6}).*',postcode)
+    search1 = re.search('^(4)(0)\d{4}$',postcode)
+    if search:
+        return search.group(1)
+    elif search1:
+        return search1.group(1)
+    else :
+        return '000000'
+for i in tree.getiterator('tag'):
+    k1 = i.get("k")
+    if k1 == "addr:postcode":
+        v1 = i.get("v")
+        print  update_postcode(v1)
+        
